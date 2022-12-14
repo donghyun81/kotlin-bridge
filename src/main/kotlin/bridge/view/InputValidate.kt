56e8692d9441ef6c4dd98a781dd60aa07b562db0
윤동현 <donghyunyoon81@gmail.com>
2022-12-14 09:39:24 +0900

    feat<InputValidate> : bridgeSizeForm 기능 구현
    3부터 20까지의 입력값이 아닐경우 예외처리

--- src/main/kotlin/bridge/view/InputValidate.kt
+++ src/main/kotlin/bridge/view/InputValidate.kt
@@ -2,4 +2,17 @@ package bridge.view
 
 class InputValidate() {
 
+    fun bridgeSizeForm(input: String) {
+        try {
+            require(BRIDGE_SIZE_FORM.toRegex().matches(input)) {
+            }
+        } catch (e:IllegalArgumentException){
+            println("[ERROR] 3 부터 20까지의 숫자만 입력해주세요")
+        }
+    }
+
+    companion object {
+        const val BRIDGE_SIZE_FORM = "^[3-9]$|^1[0-9]$|^20\$"
+    }
+
 }
