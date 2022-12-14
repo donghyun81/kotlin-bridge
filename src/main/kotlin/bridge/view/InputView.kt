01ba37a5363afe3154d8a66743599aad8508bf03
윤동현 <donghyunyoon81@gmail.com>
2022-12-14 10:23:33 +0900

    refactor<InputView> : readBridgeSize,readMoving,readGameCommand 기능 변경
    readBridgeSize 숫자를 입력 받아 반환 3부터 20이 아닐 경우 예외 발생
    readMoving 문자를 입력 받아 반환 U 또는 D가 아닐 경우 예외 발생
    readGameComman 문자를 입력 받아 반환 R 또는 D가 아닐 경우 예외 발생

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,27 +1,36 @@
 package bridge.view
 
+import camp.nextstep.edu.missionutils.Console
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
 class InputView {
-    /**
-     * 다리의 길이를 입력받는다.
-     */
+
+    private val inputValidate = InputValidate()
+
     fun readBridgeSize(): Int {
-        return 0
+        val bridgeSize = Console.readLine()
+        inputValidate.bridgeSizeForm(bridgeSize)
+        return bridgeSize.toInt()
     }
 
     /**
      * 사용자가 이동할 칸을 입력받는다.
      */
     fun readMoving(): String {
-        return ""
+        val moving = Console.readLine()
+        inputValidate.bridgeSizeForm(moving)
+        return moving
     }
 
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
     fun readGameCommand(): String {
-        return ""
+        val gameCommand = Console.readLine()
+        inputValidate.gameCommandForm(gameCommand)
+        return gameCommand
     }
+
 }
