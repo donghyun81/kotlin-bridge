732d643c32e38a0d040ef4e2d8bf79265e500ea9
윤동현 <donghyunyoon81@gmail.com>
2022-12-14 12:25:55 +0900

    feat<BridgeMoving> : blockLocation 기능 구현
    다리 이동 위치에 맞는 결과 반환

--- src/main/kotlin/bridge/model/BridgeMoving.kt
+++ src/main/kotlin/bridge/model/BridgeMoving.kt
@@ -2,6 +2,11 @@ package bridge.model
 
 class BridgeMoving {
 
+    fun blockLocation(block: String, moving: String): Pair<String, String> {
+        if (moving == "U") return Pair(blockResult(block, moving), BLANK)
+        return Pair(BLANK, blockResult(block, moving))
+    }
+
     fun blockResult(block: String, moving: String): String {
         if (block == moving) return PASS
         return FAIL
