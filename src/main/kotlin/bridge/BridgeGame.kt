c63d56974498831d335fe2d9c95064094e9fdb30
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-24 13:47:59 +0900

    feat(BridgeGame): 다리를 이동하는 기능 추가

--- src/main/kotlin/bridge/BridgeGame.kt
+++ src/main/kotlin/bridge/BridgeGame.kt
@@ -20,7 +20,33 @@ class BridgeGame {
      *
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    fun move() {}
+    fun move(bridge: List<String>): Boolean {
+        val upSquares = mutableListOf<String>()
+        val downSquares = mutableListOf<String>()
+        var movingResult: MovingResult
+        bridge.forEach { square ->
+            val userMovingInput = inputView.readMoving()
+            movingResult = MovingResult(userMovingInput, square == userMovingInput)
+            addUpSquareStringBuilder(upSquares, downSquares, movingResult)
+            outputView.printMap(upSquares, downSquares)
+            if (movingResult.isMovingSuccess.not()) return movingResult.isMovingSuccess
+        }
+        return true
+    }
+
+    private fun addUpSquareStringBuilder(
+        upSquares: MutableList<String>,
+        downSquares: MutableList<String>,
+        movingResult: MovingResult
+    ) {
+        if (movingResult.moveDirection == "U") {
+            upSquares.add(movingResult.getMovingText())
+            downSquares.add("   ")
+            return
+        }
+        upSquares.add("   ")
+        downSquares.add(movingResult.getMovingText())
+    }
 
     /**
      * 사용자가 게임을 다시 시도할 때 사용하는 메서드
