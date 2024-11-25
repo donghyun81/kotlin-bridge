fcd2ba8b28f274f01fab630c425b2f8c23c3a7ab
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-11-25 18:57:46 +0900

    feat(OutputView): 총 게임 결과를 반환하는 기능 추가

--- src/main/kotlin/bridge/OutputView.kt
+++ src/main/kotlin/bridge/OutputView.kt
@@ -22,6 +22,18 @@ class OutputView {
      * 게임의 최종 결과를 정해진 형식에 맞춰 출력한다.
      *
      * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
+     * 최종 게임 결과
+     * [ O | X ]
+     * [   |   ]
+     *
+     * 게임 성공 여부: 실패
+     * 총 시도한 횟수: 1
      */
-    fun printResult() {}
+    fun printResult(bridgeResult: BridgeResult, tryCount: Int) {
+        println("최종 게임 결과")
+        println("[${bridgeResult.getUpSquares().joinToString("|")}]")
+        println("[${bridgeResult.getDownSquares().joinToString("|")}]")
+        println("게임 성공 여부: ${bridgeResult.gameResult()}")
+        println("총 시도한 횟수: $tryCount")
+    }
 }
